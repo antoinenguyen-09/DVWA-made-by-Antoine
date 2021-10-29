0dddf2b575ad2b0ac463e200a69b8d1c97e48c15
Nguyễn Cao Huy Hoàng <61876488+antoinenguyen-09@users.noreply.github.com>
2021-10-29 15:58:23 +0700

    Update gen_mark.py

--- Command and SQL Injection/gen_mark.py
+++ Command and SQL Injection/gen_mark.py
@@ -4,7 +4,7 @@ import sys
 studentID = sys.argv[1]
 classID = sys.argv[2]
 
-mydb = mysql.connector.connect(host="localhost",user="kali",password="kali",database="class")
+mydb = mysql.connector.connect(host="",user="",password="",database=") # servername, username, password, databasename (database = "class")
 mycursor = mydb.cursor()
 mycursor.execute("SELECT mark FROM "+classID+" WHERE id="+studentID)
 myresult = mycursor.fetchall()
