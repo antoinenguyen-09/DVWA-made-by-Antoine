ca399ad87012584598b47184a3866dd0ce41e102
Nguyễn Cao Huy Hoàng <61876488+antoinenguyen-09@users.noreply.github.com>
2021-10-29 16:17:52 +0700

    Create create_class.sql

---
+++ Command and SQL Injection/create_class.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE IF NOT EXISTS class;
+USE class;
+CREATE TABLE IA1501 (
+  id INT(6) unsigned NOT NULL PRIMARY KEY,
+  fullname VARCHAR(30) NOT NULL,
+  mark FLOAT
+)
+
+CREATE TABLE IA1502 (
+  id INT(6) unsigned NOT NULL PRIMARY KEY,
+  fullname VARCHAR(30) NOT NULL,
+  mark FLOAT
+)
+
+-- insert data in 2 tables above
